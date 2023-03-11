383c66483a96a3aa084c818c38a9e833f4956823
Oleksandr Lytvyniuk <sandro132015@gmail.com>
2023-03-11 21:55:36 +0200

    add redux toolkit

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -24,6 +24,18 @@ export const Contacts = ({ contacts, receiveID }) => {
   );
 };
 
+Contacts.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
+
+  receiveID: PropTypes.func.isRequired,
+};
+
 // Contacts.propTypes = {
 //   contacts: PropTypes.arrayOf(
 //     PropTypes.shape({
