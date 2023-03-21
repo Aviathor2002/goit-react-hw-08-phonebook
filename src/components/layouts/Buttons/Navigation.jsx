0cd4951847234b1cf9ec05818b20a9a0ce4b464b
Oleksandr Lytvyniuk <sandro132015@gmail.com>
2023-03-21 10:26:55 +0200

    button settings

--- src/components/layouts/Buttons/Navigation.jsx
+++ src/components/layouts/Buttons/Navigation.jsx
@@ -1,31 +1,33 @@
 import * as React from 'react';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import PersonIcon from '@mui/icons-material/Person';
-import { Item, Link, List } from './Navigation.styled';
+import { Container, Item, Link, List } from './Navigation.styled';
 
 export const Navigation = () => {
   return (
-    <List>
-      <Item>
-        <Link
-          to={'/'}
-          style={({ isActive }) => {
-            if (isActive) return { color: 'purple' };
-          }}
-        >
-          <PersonIcon />
-        </Link>
-      </Item>
-      <Item>
-        <Link
-          to={'/addContact'}
-          style={({ isActive }) => {
-            if (isActive) return { color: 'purple' };
-          }}
-        >
-          <PersonAddIcon />
-        </Link>
-      </Item>
-    </List>
+    <Container>
+      <List>
+        <Item>
+          <Link
+            to={'/'}
+            style={({ isActive }) => {
+              if (isActive) return { color: 'purple' };
+            }}
+          >
+            <PersonIcon />
+          </Link>
+        </Item>
+        <Item>
+          <Link
+            to={'/addContact'}
+            style={({ isActive }) => {
+              if (isActive) return { color: 'purple' };
+            }}
+          >
+            <PersonAddIcon />
+          </Link>
+        </Item>
+      </List>
+    </Container>
   );
 };
--- src/components/layouts/Buttons/Navigation.styled.jsx
+++ src/components/layouts/Buttons/Navigation.styled.jsx
@@ -1,6 +1,12 @@
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
+export const Container = styled.div`
+  position: relative;
+  left: 0;
+  bottom: 0;
+`;
+
 export const List = styled.ul`
   list-style-type: none;
   margin: 0;
@@ -15,18 +21,28 @@ export const Item = styled.li`
 `;
 
 export const Link = styled(NavLink)`
+  width: 100px;
   display: block;
-  color: #666;
+  color: white;
+  background-color: #666;
   text-align: center;
   padding: 14px 16px;
   text-decoration: none;
+  border-radius: 4px;
+  box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
+    0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
 
   &:hover:not(.active) {
     background-color: #ddd;
+    transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+      box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+      border-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+      color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+    color: #fff;
   }
 
   &.active {
     color: white;
-    background-color: #4caf50;
+    background-color: #1976d2;
   }
 `;
